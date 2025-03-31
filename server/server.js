0fa75ebf3a1229400adeba7115b72a2b2266f208
Vansh Tyagi <vanshjii021@gmail.com>
2025-03-31 16:28:26 +0530

    changes

--- server/server.js
+++ server/server.js
@@ -28,7 +28,7 @@ app.use(express.urlencoded({ extended: true })); // Parse URL-encoded payloads
 app.use(cookieParser()); // Parse cookies
 app.use(
     cors({
-        origin: "http://localhost:5173",
+        origin: "https://flashlearn-1.onrender.com",
         credentials: true,
     })
 );
